17e1b35f7eb3f9eed2d9bc8a77bb29f624cbb437
HenriqueAMascarin <henriqueamascarin@gmail.com>
2024-08-29 09:28:08 -0300

    find-solution-scroll-&working

--- src/components/Timer/ListTimer.tsx
+++ src/components/Timer/ListTimer.tsx
@@ -3,7 +3,7 @@ import { Animated, NativeSyntheticEvent, NativeScrollEvent } from "react-native"
 import { heightItem } from "./styles/timerStyle";
 import { useAppSelector } from "../Utils/Redux/reduxHookCustom";
 import { numberList } from "./timerUtils/numberList";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 interface ListTimer {
     timerData: {
         maxNumber: number;
@@ -51,7 +51,7 @@ export default function ListTimer({ timerData, opacityAnimated }: ListTimer) {
             },
         ], {
         listener: (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-            if (event.nativeEvent.contentOffset && isFirst.current == false) {
+            if (event.nativeEvent.contentOffset) {
                 const lastItemsBeforeUpdate = 15;
 
                 // timerData.maxNumber + 1, More 1 because of 0
@@ -60,44 +60,35 @@ export default function ListTimer({ timerData, opacityAnimated }: ListTimer) {
                 const lowPosToAdd = heightItem * (lastItemsBeforeUpdate);
                 const highPosToAdd = heightItem * ((arrayNumbers.length - lastItemsBeforeUpdate));
 
-                // console.log(highPosToAdd, event.nativeEvent.contentOffset.y, arrayNumbers.length)
-                // find how many times have the list of numbers duplicated
+                if ((!isInverted.current && event.nativeEvent.contentOffset.y <= lowPosToAdd) || (isInverted.current && event.nativeEvent.contentOffset.y >= highPosToAdd)) {
 
-                // tem que fazer 1 vez somente, ta fazendo mais de 1
+                    console.log('toTop antes', event.nativeEvent.contentOffset.y, highPosToAdd, isInverted.current)
 
-                if (event.nativeEvent.contentOffset.y <= lowPosToAdd) {
-
-                    console.log('toTop antes', event.nativeEvent.contentOffset.y)
-
-                    // console.log('toTop antes', event.nativeEvent.contentOffset.y)
-                    // // 
                     changeArrayNumbers((oldArray) => {
-                        isInverted.current = true;
+                        const oldInverted = isInverted.current;
 
-                        isFirst.current = true;
+                        if(!isInverted.current){
+                            isInverted.current = true;
+                        }
 
-                        return [...oldArray.slice(0, realIndexMaxNumber), ...numberList(timerData.maxNumber)].reverse()
+                        return oldInverted ? [...oldArray, ...numberList(timerData.maxNumber).reverse()] : [...oldArray].reverse();
                     });
-           
-                    // console.log('toTop depois', event.nativeEvent.contentOffset.y)
-
+                    
+                    console.log('-----------------------------------------------------------------------------')
 
                 } else if (event.nativeEvent.contentOffset.y >= highPosToAdd) {
 
                     console.log('toBottom antes', event.nativeEvent.contentOffset.y)
                     // ...oldArray.slice(realIndexMaxNumber, arrayNumbers.length)
                     changeArrayNumbers((oldArray) => {
-                        const formatOldArray = isInverted.current ? oldArray.reverse() : oldArray;
+                        const oldInverted = isInverted.current;
 
-                        isInverted.current = false;
+                        if(isInverted.current){
+                            isInverted.current = false;
+                        }
 
-                        isFirst.current = true;
-
-                        return [...formatOldArray, ...numberList(timerData.maxNumber)];
+                        return oldInverted ? [...oldArray.reverse(), ...numberList(timerData.maxNumber)] : [...oldArray, ...numberList(timerData.maxNumber)];
                     });
-
-                    console.log('toBottom depois', event.nativeEvent.contentOffset.y)
-
 
                 }
 
@@ -105,12 +96,6 @@ export default function ListTimer({ timerData, opacityAnimated }: ListTimer) {
         },
         useNativeDriver: false
     });
-
-    useEffect(() => {
-        if (timerData.maxNumber < 40) {
-            console.log(arrayNumbers)
-        }
-    }, [arrayNumbers])
 
     const renderNumber = ({ item, index }: { item: typeof arrayNumbers[0], index: number }) =>
     (
@@ -136,7 +121,7 @@ export default function ListTimer({ timerData, opacityAnimated }: ListTimer) {
             maxToRenderPerBatch={15}
             maintainVisibleContentPosition={{
                 minIndexForVisible: 0,
-            
+                autoscrollToTopThreshold: 0
             }}
             initialNumToRender={15}
             bounces={false}
