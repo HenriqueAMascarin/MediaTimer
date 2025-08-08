9d9f14ac3c95ac3999143f20ebc20cf1c9f334d5
HenriqueAMascarin <henriqueamascarin@gmail.com>
2025-08-08 15:20:08 -0300

    refact: android fixes for production & builds

--- android/app/src/main/java/com/henriqueamascarin/mediatimer/MainApplication.kt
+++ android/app/src/main/java/com/henriqueamascarin/mediatimer/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.henriqueamascarin.mediatimer;
+package com.henriqueamascarin.mediatimer
 
 import android.app.Application
 import android.content.res.Configuration
@@ -24,7 +24,7 @@ class MainApplication : Application(), ReactApplication {
           override fun getPackages(): List<ReactPackage> {
             val packages = PackageList(this).packages
             // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
+            // packages.add(MyReactNativePackage())
             return packages
           }
 
