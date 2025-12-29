c98f1e54ea08ccbc4dcabb558c7dd876e314218c
HenriqueAMascarin <henriqueamascarin@gmail.com>
2025-12-29 20:53:32 -0300

    fix: some fixes in react native svg for build errors

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,19 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-const { transformer, resolver } = config;
+  const { transformer, resolver } = config;
 
-config.transformer = {
-  ...transformer,
-  babelTransformerPath: require.resolve("react-native-svg-transformer"),
-};
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
 
-config.resolver = {
-  ...resolver,
-  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-  sourceExts: [...resolver.sourceExts, "svg"],
-};
-
-module.exports = config;
+  return config;
+})();
